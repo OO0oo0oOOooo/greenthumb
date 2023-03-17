92967c18818e836009c5c0e02269e06538e71f56
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2023-03-17 01:48:02 -0600

    Delete Editor directory

--- Editor/GTPalette.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class GTPalette : ScriptableObject
-{
-    public List<PaletteData> Palette = new List<PaletteData>();
-}
