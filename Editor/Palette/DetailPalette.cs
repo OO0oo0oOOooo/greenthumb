18f3e1b3fb64b3483da2b3df58da6ea01cdd8bc0
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2023-04-28 23:16:06 -0600

    Delete Editor directory

--- Editor/Palette/DetailPalette.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DetailPalette : ScriptableObject
-{
-    public List<DetailPaletteItem> Palette = new List<DetailPaletteItem>();
-}
